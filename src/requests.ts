b9f63462c4c46729ba0ef8d28e09b86440570dd2
lukaszvikas <lukas@truelayer.com>
2019-11-22 14:45:17 +0000

    added status

--- src/requests.ts
+++ src/requests.ts
@@ -23,7 +23,7 @@ export const createPurchase = async () => {
 };
 
 export const getPurchase = async (id: string) => {
-  const result = await axios.get(`http://localhost:3000/purchases/1`);
+  const result = await axios.get(`http://localhost:8080/purchases/${id}`);
 
   if (result.data) return result.data;
 
